1d889b1483fccf5327b0af5b64e7160c514a226b
hachiko-8ko <38965940+hachiko-8ko@users.noreply.github.com>
2022-06-07 15:07:23 -0400

    chunk() should allow size 1

--- src/Queryable/Chunk.ts
+++ src/Queryable/Chunk.ts
@@ -1,11 +1,11 @@
 import { Enumerable } from '../Enumerable/Enumerable';
 
 /**
- * chunk_q_: splits the elements of a sequence into chunks of size at most size
+ * chunk_q_: splits the elements of a sequence into chunks of size at most "size"
  */
 export function chunk<T>(this: Enumerable<T>, size: number): Enumerable<Array<T>> {
     const newenum = this._extend<Array<T>>(function* _chunk(data: IterableIterator<T>): IterableIterator<Array<T>> {
-        if (!size || size <= 1) {
+        if (!size || size < 1) {
             throw new Error("Argument out of range");
         }
         let counter = size;
